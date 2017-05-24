63e8d3dd8895c97b99b196b94c53fdc356ffc9ef
frangaliana <fjgalianacano@gmail.com>
2017-05-24 17:17:11 +0200

    TIC-O Añadidas peticiones estándar para la API

--- index.js
+++ index.js
@@ -10,6 +10,24 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded( {extended:false}));
 app.use(bodyParser.json());
 
+//Manejo de errores por parte del middleware
+app.use(function(err, req, res, next) {
+  res.status(400);
+	res.end();
+});
+
+//Petición por defecto para mostrar una Home de la API
+app.get('/api', function(req, res){
+	res.status(200);
+	res.send('Bienvenido a la API REST de Thingy');
+})
+
+//Petición para dar error a entrada vacía incorrecta
+app.get('*', function(req, res){
+	res.status(404);
+	res.send('Petición incorrecta');
+})
+
 app.listen(port, () => {
   console.log(`API REST OF THINGY corriendo en http://localhost:${port}`)
 })
