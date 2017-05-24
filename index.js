7deb4f2d2f8ed2906243aff574f694baf5863340
frangaliana <fjgalianacano@gmail.com>
2017-05-24 16:25:08 +0200

    TIC-0 Añadida dependencia body-parser y nodemon Se añade dichas dependencias para tratar las peticiones POST y además no tener que iniciar el servidor cada vez para ver si funciona todo correctamente

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+//Utilizaremos la variable para que sea una variable de entorno o 3000 sino
+const port = process.env.PORT || 3000
 
-app.listen(3000, () => {
-  console.log('API REST OF THINGY corriendo en http://localhost:3000')
+app.use(bodyParser.urlencoded( {extended:false}));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`API REST OF THINGY corriendo en http://localhost:${port}`)
 })
