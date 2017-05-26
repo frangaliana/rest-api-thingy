00a8d2bd255075a40cb274566633ab7256aa790b
frangaliana <fjgalianacano@gmail.com>
2017-05-26 15:17:26 +0200

    TIC-1 Creada seguridad por JWT Hemos refactorizado la aplicación, incorporando el login y registro por JSON Web Token además probado vía Productos dichas autorizaciones

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,24 @@
 
 const express = require('express');
 const productCtrl = require('../controllers/product');
+const userCtrl = require('../controllers/user');
+const auth = require('../middlewares/auth');
 const api = express.Router();
 
+//Rutas de productos
 api.get('/product', productCtrl.getProducts);
 api.get('/product/:productId', productCtrl.getProduct);
-api.post('/product', productCtrl.saveProduct);
-api.put('/product/:productId', productCtrl.updateProduct);
-api.delete('/product/:productId', productCtrl.deleteProduct);
+api.post('/product', auth, productCtrl.saveProduct);
+api.put('/product/:productId', auth, productCtrl.updateProduct);
+api.delete('/product/:productId', auth, productCtrl.deleteProduct);
+
+//Rutas de users
+api.post('/signup', userCtrl.signUp);
+api.post('/signin', userCtrl.signIn);
+
+//Ejemplo para probar autentificación
+api.get('/private', auth, (req, res) => {
+  res.status(200).send({ message: 'Tienes acceso' });
+});
 
 module.exports = api;
